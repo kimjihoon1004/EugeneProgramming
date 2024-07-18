2fb9ba9c81c3fb86928a56fa72ccf7c913b96575
kimjihoon1004 <jihoon4545@gmail.com>
2024-07-18 17:29:27 +0900

    Starting SpringSecurity process

---
+++ EugeneProgramming/src/test/java/com/eugeneprogram/EugeneProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugeneprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EugeneProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
