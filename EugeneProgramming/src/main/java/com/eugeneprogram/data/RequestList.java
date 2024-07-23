003b5927d377b99b3d0fbc5f98bfb5e7c6555b6d
amooge <125438713+amooge@users.noreply.github.com>
2024-07-23 17:00:24 +0900

    페이지 네이션을 위한 디펜던시 추가 'data'페키지 추가 와 RequestList클래스 추가.

---
+++ EugeneProgramming/src/main/java/com/eugeneprogram/data/RequestList.java
@@ -0,0 +1,12 @@
+package com.eugeneprogram.data;
+
+import org.springframework.data.domain.Pageable;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class RequestList<T> {
+	private T data;
+	private Pageable pageable;
+}
